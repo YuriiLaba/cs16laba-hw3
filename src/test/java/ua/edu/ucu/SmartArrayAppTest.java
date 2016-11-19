792926df5ce6ec5d34f12b28b4f9442b169b6cca
YuriyVilnitskiy <laba@ucu.edu.ua>
2016-11-19 13:49:24 +0200

    DecoratorAndStrategy

--- src/test/java/ua/edu/ucu/SmartArrayAppTest.java
+++ src/test/java/ua/edu/ucu/SmartArrayAppTest.java
@@ -67,7 +67,8 @@ public class SmartArrayAppTest {
         assertEquals(expectedRes, sa.operationDescription());
 
         sa = new MapDecorator(sa, func);
-        expectedRes = "This is BaseArray, plus distinct decorator, plus filter decorator, plus sort decorator, plus map decorator";
+        expectedRes = "This is BaseArray, plus distinct decorator, plus filter decorator, plus sort " +
+                "decorator, plus map decorator";
         assertEquals(expectedRes, sa.operationDescription());
     }
 
@@ -84,6 +85,42 @@ public class SmartArrayAppTest {
 
         sa = new DistinctDecorator(sa);
         expectedRes = 6;
+        assertEquals(expectedRes, sa.size());
+
+        sa = new FilterDecorator(sa, pr);
+        expectedRes = 6;
+        assertEquals(expectedRes, sa.size());
+
+        sa = new SortDecorator(sa, cmp);
+        expectedRes = 3;
+        assertEquals(expectedRes, sa.size());
+
+        sa = new MapDecorator(sa, func);
+        expectedRes = 3;
+        assertEquals(expectedRes, sa.size());
+    }
+    @Test
+    public void testStudentSize(){
+        Student[] students = {
+                new Student("Ivar", "Grimstad", 3.9, 2),
+                new Student("Ittai", "Zeidman", 4.5, 1),
+                new Student("Antons", "Kranga", 4.0, 2),
+                new Student("Burr", "Sutter", 4.2, 2),
+                new Student("Philipp", "Krenn", 4.3, 3),
+                new Student("Tomasz", "Borek", 4.1, 2),
+                new Student("Ittai", "Zeidman", 4.5, 1),
+                new Student("Burr", "Sutter", 4.2, 2)};
+        MyPredicate pr = t -> (((Student)t).getYear() == 2 && ((Student)t).getGPA() >= 4);
+        MyComparator cmp = (o1, o2) -> (((Student) o1).getSurname().compareTo(((Student) o2).getSurname()));
+        MyFunction func = t -> ((Student) t).getSurname() + " " + ((Student) t).getName();
+
+
+        SmartArray sa = new BaseArray(students);
+        int expectedRes = 8;
+        assertEquals(expectedRes, sa.size());
+
+        sa = new DistinctDecorator(sa);
+        expectedRes = 8;
         assertEquals(expectedRes, sa.size());
 
         sa = new FilterDecorator(sa, pr);
